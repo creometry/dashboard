33f8db8682284e6e9f3a2b0c9c17e038ca32500e
Seifbarouni <seif.barouni11@gmail.com>
2022-06-10 03:58:24 -0400

    added sidebar + navbar components

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export const Navbar = () => {
+    const { REACT_APP_NAMESPACE } = process.env;
+    return (
+        <div className="bg-zinc-800 h-12 flex justify-between items-center  px-12">
+            <div className="bg-creo text-gray-200 px-3 py-1 rounded-md cursor-pointer font-bold">
+                Namespace : {REACT_APP_NAMESPACE}
+            </div>
+            <div className="flex items-center">
+                <div>
+                    <button
+                        type="button"
+                        className="bg-zinc-700 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                    >
+                        <span className="sr-only">View notifications</span>
+                        <svg
+                            className="h-6 w-6"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="2"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                            />
+                        </svg>
+                    </button>
+                </div>
+                <div className="ml-4">
+                    <button
+                        type="button"
+                        className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                        id="user-menu-button"
+                        aria-expanded="false"
+                        aria-haspopup="true"
+                    >
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                            className="h-8 w-8 rounded-full"
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            alt=""
+                        />
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
