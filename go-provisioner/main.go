301b890292dc182e0a043066b8fbc6a688779657
Seif Barouni <seif.barouni11@gmail.com>
2022-07-18 19:24:11 +0100

    Update main.go

--- go-provisioner/main.go
+++ go-provisioner/main.go
@@ -16,7 +16,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000",
+		AllowOrigins: "http://localhost:3000, http://localhost:8001",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
